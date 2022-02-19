5e26d0515077ef4bb4827bf0483e2f08e1c9a334
vigli-Lee <wnsqjadl@gmail.com>
2022-02-19 23:52:02 +0900

    add Gender Screen

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mobile.core.navigation.Route
+import com.mobile.onboarding_presentation.gender.GenderScreen
 import com.mobile.onboarding_presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +36,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
---
+++ onboarding/onboarding_presentation/src/main/java/com/mobile/onboarding_presentation/gender/GenderViewModel.kt
@@ -0,0 +1,44 @@
+/**
+ * Created by Vigli on 19,2월,2022
+ * Kmong.com
+ */
+
+package com.mobile.onboarding_presentation.gender
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobile.core.domain.model.Gender
+import com.mobile.core.domain.preferences.Preferences
+import com.mobile.core.navigation.Route
+import com.mobile.core.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GenderViewModel @Inject constructor(
+    private val preferences: Preferences
+): ViewModel() {
+
+    var selectedGender by mutableStateOf<Gender>(Gender.Male)
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onGenderClick(gender: Gender) {
+        selectedGender = gender
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            preferences.saveGender(selectedGender)
+            _uiEvent.send(UiEvent.Navigate(Route.AGE))
+        }
+    }
+}
